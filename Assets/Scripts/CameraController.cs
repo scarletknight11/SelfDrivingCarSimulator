661c84631af00270c7b322c6ec85a3e5870c1680
Sanjit Singh <sanjitpsingh@yahoo.com>
2020-12-22 20:35:17 -0500

    collision problem mostly fixed. car will not collide with pediatsrians. also implemented a camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+ 
+
+    void Start()
+    {
+	 
+    }
+    float inputX, inputZ;
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+        if (inputX != 0)
+        {
+            rotate();
+        }
+        if (inputZ != 0)
+        {
+            move();
+        }
+    }
+
+    private void move()
+    {
+        transform.position += transform.forward * inputZ * Time.deltaTime;  
+    }
+
+    private void rotate()
+    {
+        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime, 0f));
+    }
+}
